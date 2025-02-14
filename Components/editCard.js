b051d5344efa0f7121d80a0fd5f0a1af91c8a92f
Luan-Flores <keeganluan919@gmail.com>
2025-02-14 09:29:24 -0400

    FrontEnd completamente finalizado.

--- Components/editCard.js
+++ Components/editCard.js
@@ -1,23 +1,37 @@
 
+function teste(){
+    console.log("teste");
+}
 const editCard = (element) => {
     return `
-        <a href="./produto.html?id=${element.id}">
-            <div class="produto">
-                <div class="divImgProd">
-                    <img id="imgProduto" src="${element.image}">
-                </div>
-                    
-                <div class="desc">
-                    <div class="card3">
-                        <p id="nomeProduto">${element.title}</p>
+        <div class="produto">
+            <div class="infoProduto">
+                <a href="./produto.html?id=${element.id}" class="info-a">
+                    <div class="divImgProd">
+                        <img class="imgProduto" src="${element.image}">
                     </div>
-                    <div class="card4">
-                        <p id="precoProduto">R$ ${element.price}</p>
+                        
+                    <div class="desc">
+                        <div class="card3">
+                            <p class="nomeProduto">${element.title}</p>
+                        </div>
+                        <div class="card4">
+                            <p class="precoProduto">R$ ${element.price}</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
-        </a>
+            <div class="ctaCardProduto">
+                <a href="./editarProduto.html" class="main-box">
+                    <img src="../../Icons/LAPIS.png" alt="img" width="34px" height="34px">
+                    <span>Editar</span>
+                </a>
+                <a href="./remover" class="del-box">
+                    <img src="../../Icons/lixo.png" alt="" width="34px" height="34px">
+                    <span>Remover</span>
+                </a>
+            </div>
+        </div>
     `;
 };
-
 export default editCard;  // 🔹 Certifique-se de que está exportando como default
