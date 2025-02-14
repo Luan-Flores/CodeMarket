3c6cbcbaff0915bf9a77022b0ecf2db7f52c0b66
Lorena.L.P <loryfpinheiro@gmail.com>
2025-02-14 01:45:44 -0400

    Arrumei o caminho dos icons e coloquei um link no nome da loja

--- Components/headerComp.js
+++ Components/headerComp.js
@@ -2,11 +2,11 @@ const header = () => {
     
     return `
             <div class="header-container">
-                <h1>STORE</h1>
+                <h1><a href="../../../Pages/home/home.html">STORE<a/></h1>
                 <nav>
-                    <a href="../../Pages/home/home.html"><img src="../../Icons/Home.png" alt="Home"></a>
-                    <a href="#"><img src="../../Icons/Heart.png" alt="Favoritos"></a>
-                    <a href="../../Pages/Carrinho/FCSemID/index.html?acao=SI"><img src="../../Icons/Carrinho.png" alt="Carrinho"></a>
+                    <a href="../../Pages/home/home.html"><img src="../../../Icons/Home.png" alt="Home"></a>
+                    <a href="#"><img src="../../../Icons/Heart.png" alt="Favoritos"></a>
+                    <a href="../../Pages/Carrinho/FCSemID/index.html?acao=SI"><img src="../../../Icons/Carrinho.png" alt="Carrinho"></a>
                 </nav>
             </div>
     `;
